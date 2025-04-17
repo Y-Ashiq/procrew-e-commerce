9e0df121b7cd31a5da3d6a75ebbb24070a314a20
Y-Ashiq <y.alashiq11@gmail.com>
2025-04-17 20:03:04 +0200

    run on docker

--- database/DBconnection.js
+++ database/DBconnection.js
@@ -1,23 +1,11 @@
 import { Sequelize } from "sequelize";
-import 'dotenv/config';
+import "dotenv/config";
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, 
-      },
-    },
-    
-  }
-);
+const sequelize = new Sequelize("ecommerce", "postgres", "postgres", {
+  host: "postgres-db",
+  dialect: "postgres",
+  logging: false,
+});
 
 // 'postgres-db' || 'postgres' ||
 
